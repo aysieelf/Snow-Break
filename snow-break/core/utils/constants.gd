62bce56bed8c733957f88b3990f7b57d73640895
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 18:33:46 +0200

    add: constants.gd with:
    - SAVE_FILE_PATH constant
    - window size constants
    - max top scores constant

---
+++ snow-break/core/utils/constants.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# File paths
+const SAVE_FILE_PATH = "user://highscores.save"
+
+# Score system
+const MAX_TOP_SCORES = 5
+
+# Window settings
+const WINDOW_WIDTH = 800
+const WINDOW_HEIGHT = 600

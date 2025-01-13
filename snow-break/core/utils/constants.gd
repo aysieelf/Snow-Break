7a60874c71ac779db7970707aa4ed1e7ad97b22d
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-13 18:55:20 +0200

    add: constants to constants.gd:
    - MAX_MASTER_VOLUME
    - MAX_SFX_VOLUME
    - MAX_MUSIC_VOLUME

--- snow-break/core/utils/constants.gd
+++ snow-break/core/utils/constants.gd
@@ -2,6 +2,7 @@ extends Node
 
 # File paths
 const SAVE_FILE_PATH = "user://highscores.save"
+const SAVE_SOUND_SETTINGS_PATH = "user://sound_settings.save"
 
 # Score system
 const MAX_TOP_SCORES = 5
@@ -9,3 +10,8 @@ const MAX_TOP_SCORES = 5
 # Window settings
 const WINDOW_WIDTH = 800
 const WINDOW_HEIGHT = 600
+
+# Sound Manager
+const MAX_MASTER_VOLUME = 1.0
+const MAX_SFX_VOLUME = 1.0
+const MAX_MUSIC_VOLUME = 1.0

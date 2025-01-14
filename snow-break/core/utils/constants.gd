514f3a8d17ab14eb330d90a839bc86e5f8050d7b
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-14 20:02:18 +0200

    add: to constants.gd - paddle settings

--- snow-break/core/utils/constants.gd
+++ snow-break/core/utils/constants.gd
@@ -15,3 +15,11 @@ const WINDOW_HEIGHT = 600
 const MAX_MASTER_VOLUME = 1.0
 const MAX_SFX_VOLUME = 1.0
 const MAX_MUSIC_VOLUME = 1.0
+
+# Player settings
+const PADDLE_SPEED = 5
+const PADDLE_HEIGHT = 32
+const PADDLE_WIDTH = 128
+const PADDLE_POS = (336, 568) # this should be a tuple but it isn't working
+
+# Ball settings

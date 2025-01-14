56012a5342efc026972e8b06dd7300220feafe4c
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-14 20:08:45 +0200

    add: to constants.gd - ball settings

--- snow-break/core/utils/constants.gd
+++ snow-break/core/utils/constants.gd
@@ -17,9 +17,24 @@ const MAX_SFX_VOLUME = 1.0
 const MAX_MUSIC_VOLUME = 1.0
 
 # Player settings
-const PADDLE_SPEED = 5
+const PADDLE_SPEED = 5 # TODO: this is a placeholder for now
 const PADDLE_HEIGHT = 32
 const PADDLE_WIDTH = 128
-const PADDLE_POS = (336, 568) # this should be a tuple but it isn't working
+const PADDLE_POS = Vector2(336, 568) 
+const PADDLE_MIN_X = 0
+const PADDLE_MAX_X = 672
 
 # Ball settings
+const BALL_INIT_SPEED = 5 # TODO: this is a placeholder for now
+const BALL_MAX_SPEED = 10
+const BALL_ACCELERATION = 0.1
+const BALL_HEIGHT = 16
+const BALL_WIDTH = 16
+const BALL_POS = Vector2(392, 552)
+const BALL_INIT_VECTOR = Vector2(0, -1) # up
+const MIN_BOUNCE_ANGLE = deg_to_rad(-60)
+const MAX_BOUNCE_ANGLE = deg_to_rad(60)
+const BALL_MIN_X = 0
+const BALL_MAX_X = 784
+const BALL_MIN_Y = 0
+const BALL_MAX_Y = 584

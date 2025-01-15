a9d37dbd4cfea63c0924e835bd484c23af4e58d6
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-15 11:01:42 +0200

    update: window_scaler.gd - remove debugging prints

--- snow-break/core/managers/window_scaler.gd
+++ snow-break/core/managers/window_scaler.gd
@@ -2,16 +2,8 @@ extends Node
 
 func _ready():
 	var window = get_window()
-	var scale_factor = window.get_content_scale_factor()
-	print("Scale factor:", scale_factor)
-	
 	var dpi = DisplayServer.screen_get_dpi()
-	print("Screen DPI:", dpi)
-	
 	var pixel_size = window.get_size()
-	print("Current window size:", pixel_size)
-	
-	# На Mac Retina дисплеите обикновено са с DPI > 180
+	# On MacOS Retina displays are usually DPI > 180
 	if dpi > 180:
 		window.set_size(Vector2i(pixel_size.x * 2, pixel_size.y * 2))
-		print("New window size:", window.get_size())

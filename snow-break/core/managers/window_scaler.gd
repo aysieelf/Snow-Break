506eafee4a6b7bf058985ed75a69d6cdfba60599
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-15 10:33:11 +0200

    add: the window scaler script to ready() func

--- snow-break/core/managers/window_scaler.gd
+++ snow-break/core/managers/window_scaler.gd
@@ -2,8 +2,12 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	var window = get_window()
+	var scale_factor = window.get_content_scale_factor()
+	var pixel_size = window.get_size()
+	if scale_factor == 2.0:
+		window.set_size(Vector2i(pixel_size.x * 2, pixel_size.y * 2))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

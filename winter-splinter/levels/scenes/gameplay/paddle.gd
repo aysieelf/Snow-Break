5d1175b5ffded72685726a8866bbf7506a6a6d10
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-23 13:52:53 +0200

    update: paddle.gd - change face or color of paddle depending on signals about levelup or game over screen

--- winter-splinter/levels/scenes/gameplay/paddle.gd
+++ winter-splinter/levels/scenes/gameplay/paddle.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-## shouldn't go outside game screen
-## should have a collision box around itself
-## if game state is playing and powerup_timer is not on, should have default face
-## if game state is playing and powerup_timer is on, powerup face should be visible instead, and strengthupglow should be visible
-## if game state is level up, levelup face should be visible, and strengthupglow should be visible
-## if game state is game over, powerup face should be visible
+func _ready():
+	#GameManager.powerup_activated.connect(_on_powerup_activated)
+	#GameManager.powerup_ended.connect(_on_powerup_deactivated)
+	GameManager.level_up_screen_activated.connect(_on_level_up_screen_activated)
+	GameManager.game_over_screen_activated.connect(_on_game_over_screen_activated)
+
+	_change_paddle_face()
 
 func _physics_process(delta):
 	# Get the input direction (-1 for left, 1 for right, 0 for no input)
@@ -18,3 +19,34 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	global_position.x = clamp(global_position.x, 0, Constants.WINDOW_WIDTH - Constants.PADDLE_WIDTH)
+
+func _on_level_up_screen_activated() -> void:
+	_change_paddle_face("LevelupFace")
+	
+func _on_game_over_screen_activated() -> void:
+	_change_paddle_face("GameOverFace")
+	
+func _on_power_up_activated() -> void:
+	_change_paddle_face("PowerupFace")
+	
+func _on_power_up_deactivated() -> void:
+	_change_paddle_face("DefaultFace")
+
+func _change_paddle_face(face: String = "DefaultFace") -> void:
+	_hide_all_faces()
+	if face == "DefaultFace":
+		$DefaultFace.visible = true
+	elif face == "LevelupFace":
+		$LevelupFace.visible = true
+		$StrengthUpGlow.visible = true
+	elif face == "PowerupFace":
+		$PowerupFace.visible = true
+		$StrengthUpGlow.visible = true
+	elif face == "GameOverFace":
+		$PowerupFace.visible = true
+
+func _hide_all_faces() -> void:
+	$DefaultFace.visible = false
+	$LevelupFace.visible = false
+	$PowerupFace.visible = false
+	$StrengthUpGlow.visible = false

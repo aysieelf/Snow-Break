f5adbe2c86504f18c4951bc6e0ed8376cf6f24a0
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-23 15:56:54 +0200

    update: snowball.gd - check if colliding with strong brick - check for signals if powerup active - set ball strength

--- winter-splinter/levels/scenes/gameplay/snowball.gd
+++ winter-splinter/levels/scenes/gameplay/snowball.gd
@@ -3,20 +3,33 @@ extends CharacterBody2D
 var speed = Constants.BALL_INIT_SPEED
 var direction = Constants.BALL_INIT_VECTOR
 var is_moving = false
-
+var ball_strength = 1
 
 func _ready():
+	#GameManager.powerup_activated.connect(_on_powerup_activated)
+	#GameManager.powerup_ended.connect(_on_powerup_deactivated)
+	velocity = speed * direction
+	
 	reset_ball()
 
 func _physics_process(delta):
-	if is_moving:
-		velocity = direction * speed
-		move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		var collider = collision.get_collider().name
+		if "StrongBrick" in collider:
+			collider.take_damage(ball_strength)
+		velocity = velocity.bounce(collision.get_normal())
 
 func launch():
 	is_moving = true
 
 func reset_ball():
 	position = Constants.BALL_POS
-	is_moving = false
 	direction = Constants.BALL_INIT_VECTOR
+	is_moving = true
+
+func _on_power_up_activated() -> void:
+	ball_strength = 3
+	
+func _on_power_up_deactivated() -> void:
+	ball_strength = 1

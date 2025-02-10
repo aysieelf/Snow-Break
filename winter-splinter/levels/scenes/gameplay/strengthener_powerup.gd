ff1f872c93bc32760cbe59653d4b13c8e190ec23
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-02-10 14:14:34 +0200

    update: strengthener_powerup.gd - remove powerup if it's off screen

--- winter-splinter/levels/scenes/gameplay/strengthener_powerup.gd
+++ winter-splinter/levels/scenes/gameplay/strengthener_powerup.gd
@@ -5,6 +5,8 @@ var collected: bool = false
 
 func _process(delta):
 	position.y += fall_speed * delta
+	if position.y > (get_viewport_rect().size.y + 50):
+		queue_free()
 
 func _on_body_entered(body):
 	if body.is_in_group("paddle"):

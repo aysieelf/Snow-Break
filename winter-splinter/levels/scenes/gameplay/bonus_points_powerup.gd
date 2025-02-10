8443e9f7cc2cf1ed490b588805d7547e209ff1ad
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-02-10 13:46:24 +0200

    update: bonus_points_powerup.gd - define on_body_entered func

--- winter-splinter/levels/scenes/gameplay/bonus_points_powerup.gd
+++ winter-splinter/levels/scenes/gameplay/bonus_points_powerup.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+var fall_speed: float = Constants.FALL_SPEED
+var collected: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(delta):
+	position.y += fall_speed * delta
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body):
+	if body.is_in_group("paddle"):
+		collected = true
+		# Apply effect: Increase score
+		ScoreManager.add_points(Constants.POWERUP_POINTS)
+		queue_free()

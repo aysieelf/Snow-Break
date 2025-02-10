457fb39a67a8f9afdcde1052577bf55fef2567d7
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-02-10 17:09:02 +0200

    update: normal_brick.gd - add sound effect

--- winter-splinter/levels/scenes/gameplay/normal_brick.gd
+++ winter-splinter/levels/scenes/gameplay/normal_brick.gd
@@ -8,4 +8,5 @@ func take_damage(damage):
 	$BrickBreak.emitting = true
 	$BrickBreak.process_mode = Node.PROCESS_MODE_ALWAYS
 	$BrickBreak.reparent(get_parent()) 
+	SoundManager.play_sfx("brick-break")
 	queue_free()  

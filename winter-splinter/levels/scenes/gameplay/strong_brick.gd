a64c61127f32722181731786ea3f90f57df0c31b
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-02-10 17:10:08 +0200

    update: strong_brick.gd - add sound effect

--- winter-splinter/levels/scenes/gameplay/strong_brick.gd
+++ winter-splinter/levels/scenes/gameplay/strong_brick.gd
@@ -9,13 +9,16 @@ func _ready() -> void:
 func take_damage(damage):
 	current_health -= damage
 	if current_health == 2:
+		SoundManager.play_sfx("snowball-bounce")
 		$HealthBreakTwo.visible = true
 	elif current_health == 1:
+		SoundManager.play_sfx("snowball-bounce")
 		$HealthBreakOne.visible = true
 	elif current_health <= 0:
 		$BrickBreak.emitting = true
 		$BrickBreak.process_mode = Node.PROCESS_MODE_ALWAYS
 		$BrickBreak.reparent(get_parent()) 
+		SoundManager.play_sfx("brick-break")
 		queue_free() 
 		
 func reset_brick() -> void:

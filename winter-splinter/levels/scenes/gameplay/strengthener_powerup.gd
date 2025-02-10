d94782e6a8e2a3de34b154128675a85105dadf56
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-02-10 15:48:58 +0200

    update: strengthener_powerup.gd - adds 2 points when collected

--- winter-splinter/levels/scenes/gameplay/strengthener_powerup.gd
+++ winter-splinter/levels/scenes/gameplay/strengthener_powerup.gd
@@ -3,6 +3,9 @@ extends Area2D
 var fall_speed: float = Constants.FALL_SPEED
 var collected: bool = false
 
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
 func _process(delta):
 	position.y += fall_speed * delta
 	if position.y > (get_viewport_rect().size.y + 50):
@@ -11,6 +14,6 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("paddle"):
 		collected = true
-		# Apply effect: Increase ball strength
 		GameManager.increase_ball_strength()
+		ScoreManager.add_points(2)
 		queue_free()
